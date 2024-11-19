279dce89057a243cab77e859d30f3bdb8cb10c3c
iansun2 <iansun2004@gmail.com>
2024-11-19 09:48:47 +0000

    new create script

--- create.sh
+++ create.sh
@@ -4,14 +4,15 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "DIR: ${DIR}"
 
 # docker pull arm64v8/ros:humble-perception
-export WAYLAND_DISPLAY=wayland-1
+export WAYLAND_DISPLAY=wayland-0
 
 docker run -it -d --name ros2 \
     --privileged \
     --net=host \
     -e XDG_RUNTIME_DIR=/tmp \
 	-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
-	-v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY \
+	-v $XDG_RUNTIME_DIR/wayland-0:/tmp/wayland-0 \
+    -v $XDG_RUNTIME_DIR/wayland-1:/tmp/wayland-1 \
 	-e QT_QPA_PLATFORM=wayland \
     -v ${DIR}/workspace:/root/workspace \
     -v ${DIR}/user_data/pip-packages:/usr/local/lib/python3.10/dist-packages \
