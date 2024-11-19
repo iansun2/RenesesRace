091342c4d3eaedbaf58f3bd00f71e342e10a2c8c
iansun2 <iansun2004@gmail.com>
2024-11-19 09:41:09 +0000

    new camera input

--- workspace/src/camera_input/camera_input/camera_node.py
+++ workspace/src/camera_input/camera_input/camera_node.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+import numpy as np
 import cv2
 import redis
 import time
@@ -11,20 +12,24 @@ class USBCameraRGBNode(Node):
         super().__init__('camera_node')
         self.get_logger().info("init start")
         # data stream
-        self.publisher_ = self.create_publisher(Image, '/rgb', 10)
+        self.publisher_ = self.create_publisher(Image, '/rgb', 1)
         self.rds = redis.Redis(host='localhost', port=6379, db=0)
         self.pipe = self.rds.pipeline()
         # rclpy 
-        self.timer = self.create_timer(0.03, self.timer_callback)
+        self.timer = self.create_timer(0.01, self.timer_callback)
         self.bridge = CvBridge()
-        self.declare_parameter('camera', 0)
-        self.declare_parameter('file', '')
-        camera = self.get_parameter('camera').get_parameter_value().integer_value
-        file = self.get_parameter('file').get_parameter_value().string_value
-        if file == '':
-            self.src = camera
+        # parameter
+        self.declare_parameter('src', '')
+        src_str = self.get_parameter('src').get_parameter_value().string_value
+        # use camera 0
+        if src_str == "":
+            self.src = 0 + cv2.CAP_V4L2
+        # is camera (number)
+        elif src_str.isnumeric():
+            self.src = int(src_str) + cv2.CAP_V4L2
+        # is filename (other)
         else:
-            self.src = file
+            self.src = src_str
         # fps counter
         self.fps = 0
         self.last_count_fps = time.time()
@@ -32,37 +37,61 @@ class USBCameraRGBNode(Node):
         self.frame_id = 0
         self.get_logger().info("init cap")
         self.get_logger().info(f"open cap: {self.src}")
-        self.cap = cv2.VideoCapture()
-        self.cap.open(self.src, apiPreference=cv2.CAP_V4L2)
-        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3) # auto expos
-        time.sleep(3)
-        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1) # manual expos
-        # self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 1)
-        self.cap.set(cv2.CAP_PROP_EXPOSURE, 80)
-        # self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-        # self.cap.set(cv2.CAP_PROP_SATURATION, 0)
+        self.cap = cv2.VideoCapture(self.src)
         if not self.cap.isOpened():
             self.get_logger().error(f"Could not open video source {self.src}")
             rclpy.shutdown()
+        # config cap when it is camera
+        if isinstance(self.src, int):
+            self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3) # auto expos
+            time.sleep(3)
+            self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1) # manual expos
+            self.cap.set(cv2.CAP_PROP_EXPOSURE, 80)
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+            self.cap.set(cv2.CAP_PROP_CONVERT_RGB, 1)
+            # self.cap.set(cv2.CAP_PROP_FPS, 10)
         self.get_logger().info("init end")
 
     def timer_callback(self):
+        dt = [[0, time.time()]]
         ret, frame = self.cap.read()
+        dt.append([dt[-1][0] + 1, time.time()])
         if ret:
+            # print("shape: ", frame.shape)
             self.fps += 1
             if time.time() - self.last_count_fps >= 1:
                 self.last_count_fps = time.time()
                 self.get_logger().info(f"{self.fps} fps, {1000 / self.fps :.2f} ms")
                 self.fps = 0
-            # frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_AREA)
-            # cv2.imshow("camera", frame)
-            cv2.waitKey(1)
-            # self.get_logger().info('new image')
-            self.rds.set('camera', frame.tostring())
+            # dt.append([dt[-1][0] + 1, time.time()])
+            ''' ros2 msg '''
             msg = self.bridge.cv2_to_imgmsg(frame, 'bgr8')
             msg.header.frame_id = str(self.frame_id)
-            self.pipe.execute()
             self.publisher_.publish(msg)
+            # dt.append([dt[-1][0] + 1, time.time()])
+            '''  to square '''
+            pad = np.zeros((int((frame.shape[1] - frame.shape[0]) / 2), frame.shape[1], 3), dtype=np.uint8)
+            img = np.concatenate((pad, frame, pad), axis=0)
+            # dt.append([dt[-1][0] + 1, time.time()])
+            # cv2.imshow("img", img)
+            ''' redis '''
+            self.pipe.set('img', img.tostring())
+            self.pipe.set('img_idx', self.frame_id)
+            self.pipe.set('img_shape', str(img.shape[0]) + " " + str(img.shape[1]))
+            self.pipe.execute()
+            dt.append([dt[-1][0] + 1, time.time()])
+            ''' end of loop '''
+            # print dt with downsample
+            if self.frame_id % 5 == 0:
+                last_dt = dt[0]
+                for t in dt:
+                    print(f"{int((t[1] - last_dt[1])*1000)}, ", end='')
+                    last_dt = t
+                print("\n================")
+            # update variable
+            self.frame_id += 1
+            cv2.waitKey(1)
         else:
             self.get_logger().error('Error reading from video device')
             self.cap = cv2.VideoCapture(self.src)
--- workspace/src/cv_test/receive.py
+++ workspace/src/cv_test/receive.py
@@ -2,22 +2,43 @@ import cv2
 import redis
 import numpy as np
 import time
+import json
 
 rds = redis.Redis(host='localhost', port=6379, db=0)
+last_img_idx = 0
 frame_cnt = 0
 last_print = time.time()
 
+img_shape = rds.get('img_shape').split()
+img_shape = (int(img_shape[0]), int(img_shape[1]))
+print("shape:", img_shape)
+
 while True:
-    try:
-        img_buff = rds.get('camera')
-        # img = np.frombuffer(img_buff, np.uint8).reshape(720, 1280, 3)
-        img = np.frombuffer(img_buff, np.uint8).reshape(480, 640, 3)
+    img_idx = rds.get('img_idx')
+    if last_img_idx != img_idx:
+        yolo_detect = rds.get('yolo_detect')
+        img_buff = rds.get('img')
+        img = np.frombuffer(img_buff, np.uint8).reshape(img_shape[0], img_shape[1], 3)
+        # process img
+        json_object = json.loads(yolo_detect)
+        detect_list = json_object['detect']
+        for detect_obj in detect_list:
+            x = detect_obj['center_x']
+            y = detect_obj['center_y']
+            w = detect_obj['box_w']
+            h = detect_obj['box_h']
+            label = detect_obj['label']
+            prob = detect_obj['prob']
+            # cv2.circle(img, (x, y), 5, (255, 0, 255), 2)
+            # cv2.rectangle(img, rec=(x, y, w, h), color=(255, 0, 0), thickness=2, lineType=cv2.LINE_8)
+
+
+        # fps counter
         frame_cnt += 1
         if time.time() - last_print >= 1:
             last_print = time.time()
             print(f"{frame_cnt} fps, {1000 / frame_cnt} ms")
             frame_cnt = 0
-        # cv2.imshow("receive", img)
-        cv2.waitKey(1)
-    except Exception as e:
-        print("error: ", e)
+        cv2.imshow("receive", img)
+        last_img_idx = img_idx
+    cv2.waitKey(1)
